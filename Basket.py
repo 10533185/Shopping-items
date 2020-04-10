b9681110070b3b2cb1339a0b3cc50b7a25a8ffbd
10533185 <55691143+10533185@users.noreply.github.com>
2020-04-10 13:21:55 +0100

    Code updated

--- Basket.py
+++ Basket.py
@@ -1,5 +1,9 @@
 Rate={'Ball-Red':5,'Ball-White':6,'Pen-Blue':2,'Paper-White':3,'Shirt-small':5,'Shirt-Med':6,'Watch-med':25,'Watch-small':15}
 
+Products={'Ball-Red':{'Description':'High Bouncing Ball','Seller':'Amazon traders','price':4,'Currency':'EUR'},
+          'Shirt-small':{'Description':'Slim fit-made for men','Seller':'John','price':3,'Currency':'USD'},
+          'Watch-med':{'Description':'Stylish Design-made for men','Seller':'Andrew','price':1,'Currency':'USD'}}
+
 class Basket:
     
     def __init__(self,b={}):
@@ -8,7 +12,8 @@ class Basket:
         
     def basketItems(self,sku):
         if sku not in self.__Items:
-            return 0
+            return 0       
+                
         return self.__Items[sku] 
     
     def addItems(self,sku,qty):
@@ -16,18 +21,30 @@ class Basket:
             raise ValueError('Negative Quantity is not permitted')
         self.__Items[sku]= self.basketItems(sku)+qty
         
-        for k,v in self.__Items.items():
+        for k,v in list(self.__Items.items()):
+            if v==0:
+                del self.__Items[k]
             print(k,v)
                       
             
         
     def removeItems(self,sku,qty):
+        for k,v in list(self.__Items.items()):
+            if v==0:
+                del self.__Items[k] 
+            
         if qty<0:
             raise ValueError('Negative Quantity is not allowed')
+            
         if self.basketItems(sku)<qty:
-            raise ValueError('Basket is Empty')
+            raise ValueError(sku +' is removed from the basket')
+        
         
         self.__Items[sku]= self.basketItems(sku)-qty
+        print(k,v)
+        
+        
+        
         
     def value(self):
         total=0
@@ -37,7 +54,8 @@ class Basket:
         return total
     
     def basketContents(self):
-        for k,v in self.__Items.items():
+        for k,v in list(self.__Items.items()):
             print(k,v)
+    
             
             
